892eb86cc0244afc854a8b792d5b5e33e2f2e71d
Saumya Ramolia <saumyaramolia@gmail.com>
2024-03-05 20:08:39 -0500

    m

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import logging
 import pickle
 import pandas as pd
 
 app = Flask(__name__)
 CORS(app, origins='*')
 
+# Configure logging
+logging.basicConfig(level=logging.ERROR)
+
 # Load the serialized model
 try:
     with open('kidney_prediction_model.pkl', 'rb') as f:
         loaded_model = pickle.load(f)
 except Exception as e:
-    print(f"Error loading model: {e}")
+    logging.error(f"Error loading model: {e}")
     loaded_model = None
 
 # Map prediction labels to human-readable output
@@ -21,10 +25,10 @@ prediction_labels = {0: "Not a patient of CKD", 1: "Patient has CKD"}
 # API endpoint for making predictions
 @app.route('/predict', methods=['POST'])
 def predict():
-    if loaded_model is None:
-        return jsonify({'error': 'Model not loaded.'}), 500
-
     try:
+        if loaded_model is None:
+            return jsonify({'error': 'Model not loaded.'}), 500
+
         # Get data from the request
         data = request.get_json()
 
@@ -40,7 +44,7 @@ def predict():
         # Return the prediction as JSON response
         return jsonify({'prediction': prediction_output})
     except Exception as e:
-        print(f"Error predicting: {e}")
+        logging.error(f"Error predicting: {e}")
         return jsonify({'error': 'An error occurred while predicting.'}), 500
 
 
