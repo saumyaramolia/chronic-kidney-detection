921182cb62469eef722e56df58e552181f43bcbf
Saumya Ramolia <saumyaramolia@gmail.com>
2024-03-05 19:33:57 -0500

    made changes to app

--- app.py
+++ app.py
@@ -12,11 +12,17 @@ try:
         loaded_model = pickle.load(f)
 except Exception as e:
     print(f"Error loading model: {e}")
+    loaded_model = None
 
+# Map prediction labels to human-readable output
+prediction_labels = {0: "Not a patient of CKD", 1: "Patient has CKD"}
 
 # API endpoint for making predictions
 @app.route('/predict', methods=['POST'])
 def predict():
+    if loaded_model is None:
+        return jsonify({'error': 'Model not loaded.'}), 500
+
     try:
         # Get data from the request
         data = request.get_json()
@@ -27,12 +33,14 @@ def predict():
         # Make prediction using the loaded model
         prediction = loaded_model.predict(input_data)
 
+        # Convert prediction to human-readable output
+        prediction_output = prediction_labels.get(prediction[0], "Unknown")
+
         # Return the prediction as JSON response
-        return jsonify({'prediction': prediction[0]})
+        return jsonify({'prediction': prediction_output})
     except Exception as e:
         print(f"Error predicting: {e}")
         return jsonify({'error': 'An error occurred while predicting.'}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
