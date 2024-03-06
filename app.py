5b34a4d9cfe7f8de67462c24c3a2866e4c4e1b5b
Saumya Ramolia <saumyaramolia@gmail.com>
2024-03-05 20:22:43 -0500

    changed API from flask to fast and requirements file

--- app.py
+++ app.py
@@ -1,52 +1,62 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import logging
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import pickle
-import pandas as pd
 
-app = Flask(__name__)
-CORS(app, origins='*')
-
-# Configure logging
-logging.basicConfig(level=logging.ERROR)
+app = FastAPI()
 
 # Load the serialized model
 try:
     with open('kidney_prediction_model.pkl', 'rb') as f:
         loaded_model = pickle.load(f)
 except Exception as e:
-    logging.error(f"Error loading model: {e}")
-    loaded_model = None
+    raise HTTPException(status_code=500, detail=f"Error loading model: {e}")
 
-# Map prediction labels to human-readable output
+# Prediction labels
 prediction_labels = {0: "Not a patient of CKD", 1: "Patient has CKD"}
 
 
-# API endpoint for making predictions
-@app.route('/predict', methods=['POST'])
-def predict():
+# Define request body
+class PredictionInput(BaseModel):
+    age: int
+    blood_pressure: int
+    specific_gravity: float
+    albumin: int
+    sugar: int
+    red_blood_cells: str
+    pus_cell: str
+    pus_cell_clumps: str
+    bacteria: str
+    blood_glucose_random: int
+    blood_urea: int
+    serum_creatinine: float
+    sodium: int
+    potassium: float
+    haemoglobin: float
+    packed_cell_volume: int
+    white_blood_cell_count: int
+    red_blood_cell_count: float
+    hypertension: str
+    diabetes_mellitus: str
+    coronary_artery_disease: str
+    appetite: str
+    peda_edema: str
+    anemia: str
+
+
+# Define prediction endpoint
+@app.post("/predict")
+def predict(input_data: PredictionInput):
     try:
-        if loaded_model is None:
-            return jsonify({'error': 'Model not loaded.'}), 500
-
-        # Get data from the request
-        data = request.get_json()
-
-        # Convert data to DataFrame
-        input_data = pd.DataFrame(data, index=[0])
+        # Convert input data to DataFrame
+        input_df = pd.DataFrame([input_data.dict()])
 
         # Make prediction using the loaded model
-        prediction = loaded_model.predict(input_data)
+        prediction = loaded_model.predict(input_df)
 
         # Convert prediction to human-readable output
         prediction_output = prediction_labels.get(prediction[0], "Unknown")
 
-        # Return the prediction as JSON response
-        return jsonify({'prediction': prediction_output})
+        # Return prediction
+        return {"prediction": prediction_output}
     except Exception as e:
-        logging.error(f"Error predicting: {e}")
-        return jsonify({'error': 'An error occurred while predicting.'}), 500
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        raise HTTPException(status_code=500, detail=f"Error predicting: {e}")
